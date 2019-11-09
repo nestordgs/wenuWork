4fd2c7992d0b421e9c684bd13616bf809b4201fb
Nestor Gutierrez <nestor.gutierrez@karibu.cl>
2019-11-09 15:06:55 -0300

    [REL] - Backend   - Se crean los archivos de configuracion para el proyecto de backend.

---
+++ server/config/db.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import bluebird from 'bluebird';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const portDb = process.env.PORT_DB || 27017
+const dbName = process.env.DBNAME
+const db = `mongodb://localhost:${portDb}/${dbName}`;
+
+mongoose.Promise = bluebird;
+
+mongoose.connect(
+  db,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  }
+);
+
+const connection = mongoose.connection;
+
+connection.on(
+  'erro',
+  console.error.bind(console, 'connection error:'
+));
+
+connection.once('open', () => {
+  console.log('Great success');
+});
