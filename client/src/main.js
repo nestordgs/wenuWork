32e48929fcd211981cae9296c0d3105d3345264c
Nestor Gutierrez <nestor.gutierrez@karibu.cl>
2019-11-09 16:15:10 -0300

    [REL] - Frontend   - Se agrega el componente bootstrap-vue para mejorar los estilos.

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,14 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
 import Vue from 'vue';
-import './plugins/bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
