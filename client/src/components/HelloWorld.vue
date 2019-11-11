a93e8508b3abee019754b08344324403e2819157
Nestor Gutierrez <nestor.gutierrez@karibu.cl>
2019-11-11 09:52:15 -0300

    [REL]   - Se modifica el home del sistema para mostrar las tecnologias utilizadas en el frontend.

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -2,30 +2,22 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      This app list characters from <a href="https://rickandmortyapi.com/documentation">The Rick and Morty API</a> and if you have an user can create a favorite list.
+      <!-- <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>. -->
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
+    <h3>Build Using</h3>
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://github.com/axios/axios" target="_blank" rel="noopener">axios</a></li>
+      <li><a href="https://bootstrap-vue.js.org/" target="_blank" rel="noopener">bootstrap-vue</a></li>
+      <li><a href="https://v-charts.js.org/#/en/README" target="_blank" rel="noopener">v-charts</a></li>
+      <li><a href="https://artemsky.github.io/vue-snotify/" target="_blank" rel="noopener">vue-snotify</a></li>
     </ul>
   </div>
 </template>
